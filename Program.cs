3ad403f4cd61cefc2e2941efc4fc05c01810bde0
Lucas Bento <lucasiguape16@gmail.com>
2023-07-18 12:11:12 -0300

    var and convertion types

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ReviewConcepts;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var idade = 10;
+        Console.WriteLine(idade);
+
+        int idade2 = 20;
+        Console.WriteLine(idade2);
+
+        string name = "lucas";
+        Console.WriteLine(name);
+
+        const int IDADE_MINIMA = 18;
+        Console.WriteLine(IDADE_MINIMA);    
+
+        //const var IDADE_MAXIMA = 60; // constantes não podem ser de tipo implícito.
+        
+        
+        // ---- converções de tipos ----
+
+        float valor;
+        int outro = 25;
+
+        valor = outro;
+        Console.WriteLine($"conversão: {valor}");
+
+        int inteiro = 100;
+        uint inteiroSemSinal = (uint)inteiro;
+        Console.WriteLine($"Conversão explícita: {inteiroSemSinal}");
+
+        float numero = 25.8f;
+        int outroNumero = (int)numero; // Pode ocorrer perca de dados.
+        Console.WriteLine($"Conversão explícita: {outroNumero}");
+    }
+}
